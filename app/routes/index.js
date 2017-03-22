896a5308614ac6583ac27b83041e7499ef043ead
brandon <bcooper085@gmail.com>
2017-03-22 11:22:44 -0700

    add emberfire and test json file with firebase

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,21 +1,7 @@
 import Ember from 'ember';
 
-var beans = [{
-    id: 1,
-    owner: "Jeff Stitches",
-    category: "Roasting",
-    body: "Why are my beans green?",
-    image: "http://wellnesspulse.com/wp-content/uploads/2013/10/Green-Coffee-Beans-54655.jpg",
-}, {
-    id: 2,
-    owner: "Rob Stitches",
-    category: "Roasting",
-    body: "Those are green beans bro.",
-    image: "",
-}]
-
 export default Ember.Route.extend({
     model() {
-        return beans;
-    }
+        return this.store.findAll('bean');
+    },
 });
--- config/environment.js
+++ config/environment.js
@@ -1,50 +1,58 @@
 /* eslint-env node */
 
 module.exports = function(environment) {
-  var ENV = {
-    modulePrefix: 'coffee-blog',
-    environment: environment,
-    rootURL: '/',
-    locationType: 'auto',
-    EmberENV: {
-      FEATURES: {
-        // Here you can enable experimental features on an ember canary build
-        // e.g. 'with-controller': true
-      },
-      EXTEND_PROTOTYPES: {
-        // Prevent Ember Data from overriding Date.parse.
-        Date: false
-      }
-    },
+    var ENV = {
+        modulePrefix: 'coffee-blog',
+        environment: environment,
+        rootURL: '/',
+        locationType: 'auto',
+        EmberENV: {
+            FEATURES: {
+                // Here you can enable experimental features on an ember canary build
+                // e.g. 'with-controller': true
+            },
+            EXTEND_PROTOTYPES: {
+                // Prevent Ember Data from overriding Date.parse.
+                Date: false
+            }
+        },
 
-    APP: {
-      // Here you can pass flags/options to your application instance
-      // when it is created
+        firebase: {
+            apiKey: "AIzaSyBJDUF_N5hhAr3DgVbF6GbSL6UVGL0OfPw",
+            authDomain: "roasted-96e28.firebaseapp.com",
+            databaseURL: "https://roasted-96e28.firebaseio.com",
+            storageBucket: "roasted-96e28.appspot.com",
+            messagingSenderId: "991923040266"
+        },
+
+        APP: {
+            // Here you can pass flags/options to your application instance
+            // when it is created
+        }
+    };
+
+    if (environment === 'development') {
+        // ENV.APP.LOG_RESOLVER = true;
+        // ENV.APP.LOG_ACTIVE_GENERATION = true;
+        // ENV.APP.LOG_TRANSITIONS = true;
+        // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
+        // ENV.APP.LOG_VIEW_LOOKUPS = true;
     }
-  };
 
-  if (environment === 'development') {
-    // ENV.APP.LOG_RESOLVER = true;
-    // ENV.APP.LOG_ACTIVE_GENERATION = true;
-    // ENV.APP.LOG_TRANSITIONS = true;
-    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    // ENV.APP.LOG_VIEW_LOOKUPS = true;
-  }
+    if (environment === 'test') {
+        // Testem prefers this...
+        ENV.locationType = 'none';
 
-  if (environment === 'test') {
-    // Testem prefers this...
-    ENV.locationType = 'none';
+        // keep test console output quieter
+        ENV.APP.LOG_ACTIVE_GENERATION = false;
+        ENV.APP.LOG_VIEW_LOOKUPS = false;
 
-    // keep test console output quieter
-    ENV.APP.LOG_ACTIVE_GENERATION = false;
-    ENV.APP.LOG_VIEW_LOOKUPS = false;
+        ENV.APP.rootElement = '#ember-testing';
+    }
 
-    ENV.APP.rootElement = '#ember-testing';
-  }
+    if (environment === 'production') {
 
-  if (environment === 'production') {
+    }
 
-  }
-
-  return ENV;
+    return ENV;
 };
